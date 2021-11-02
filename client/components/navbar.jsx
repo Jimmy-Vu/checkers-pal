b99af9f9e269b662f01b1ef889a679fd08a5c135
Jimmy-Vu <JMV3211@gmail.com>
2021-11-02 23:37:14 +0000

    Create CSS for visual configuration of HTML #1

---
+++ client/components/navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Navbar extends React.Component {
+  render() {
+    return (
+    <div className="nav-bar display-flex align-center">
+      <h1 className="title">Checkers</h1>
+    </div>
+    );
+  }
+}
+
+export default Navbar;
--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
+import Navbar from '../components/navbar';
 
 export default function Home(props) {
   return (
-    <>
-    </>
+    <Navbar></Navbar>
   );
 }
