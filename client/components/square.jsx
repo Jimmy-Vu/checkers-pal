8c1df56ed7df488f261c7b8a39efe4444fdbdd14
Jimmy-Vu <JMV3211@gmail.com>
2021-11-05 18:22:55 +0000

    Removed unused modules and constructors #1

--- client/components/square.jsx
+++ client/components/square.jsx
@@ -1,25 +1,11 @@
 import React from 'react';
 class SquareCream extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      pieceIsOn: false
-    };
-  }
-
   render() {
     return <div className="square cream"></div>;
   }
 }
 
 class SquareBlack extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      pieceIsOn: false
-    };
-  }
-
   render() {
     return <div className="square black"></div>;
   }
